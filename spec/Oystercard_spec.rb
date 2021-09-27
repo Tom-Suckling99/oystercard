424b52c3b0cf66f4dbf7aa2d7d475404f4768e34
Tom Suckling <tomsuckling@Toms-MacBook-Air.local>
2021-09-27 15:49:17 +0100

    added top up method and tests chapter 5

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -9,4 +9,13 @@ describe Oystercard do
         expect(subject.balance).to eq (0)
     end
 
+    it 'responds to top_up method with argument' do
+        expect(subject).to respond_to(:top_up).with(1).argument
+    end
+
+    it 'increases the balance by however much is topped up' do
+        card = Oystercard.new
+        card.top_up(20)
+        expect(card.balance).to eq (20)
+    end
 end
